31c2707c60358ce32981df90426445cf41d03236
TijmeW <tijme.wolters@student.graafschapcollege.nl>
2022-09-07 13:08:22 +0200

    calendar dynamisch (fullcalendar)
    Dynamische calendar gemaakt met JS library fullcalendar

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,17 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    
+    public function Chartjs(){
+        $Events = array
+                  (
+                    "0" => array
+                              (
+                               "title" => "Dierentuin uitje",
+                               "start" => "2022-9-2 8:30:00",
+                               "end" => "2022-9-2 12:45:00",
+                               )
+                  );
+        return view('fullcalendar',['Events' => $Events]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/fullcalendar", "App\Http\Controllers\Controller@Chartjs");
