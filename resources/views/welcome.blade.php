660f33993340d673c6625d6611505077ecdd2ce7
Ban <ben.berkenbosch@student.graafschapcollege.nl>
2022-09-08 11:55:42 +0200

    Added Flexbox

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,25 @@
+<<<<<<< refs/remotes/origin/main
 @extends('layout')
 @section('content')
     <p>Hello Worlds</p>
 @endsection
+=======
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    </head>
+    <body class="antialiased">
+        <div id="container">
+         <h2 class="title">Overview</h2>
+
+            <p>test</p>
+        </div>
+    </body>
+</html>
+@vite(['resources/css/app.scss', 'resources/js/app.js'])
+
+>>>>>>> Added Flexbox
