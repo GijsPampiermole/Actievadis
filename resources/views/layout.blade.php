5ac71060afd5b7d2cb24a18d0392b2dadd06ac87
Keagan <keygun2k1@gmail.com>
2022-09-15 13:29:14 +0200

    Added login route to the navbar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -26,6 +26,9 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="#">Accounts</a>
                             </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/inloggen">Inloggen</a>
+                            </li>
                         </ul>
                     </div>
                 </div>
