923a09f18304c0e387224b273d5d3f085d71b501
GijsPampiermole <gpampiermole@gmail.com>
2022-09-15 11:30:42 +0200

    Navbar fix and purple background
    - Moved the header container to sit inside the header
    - Removed purple background (vite tag)

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,13 +9,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 
-    
+
 </head>
 <body>
-    
-    <div class="container-lg">
-        <nav class="navbar navbar-expand-lg bg-light">
-            <div class="container-fluid">
+
+
+      <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-lg">
+            <div class="container-fluid" style="display:flex;">
                 <a class="navbar-brand" href="#">Actievadis</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -34,8 +35,9 @@
                     </ul>
                 </div>
             </div>
-        </nav>
-    </div>
+          </div>
+      </nav>
+
 
     <div class="container-lg">
         @yield('content')
@@ -44,6 +46,6 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-    
+
 </body>
 </html>
