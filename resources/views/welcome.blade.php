be6e473a02ae3b7c4e7622f76053d01c342c5965
Ban <ben.berkenbosch@student.graafschapcollege.nl>
2022-09-15 11:05:18 +0200

    Backup Commit, as contingency when things go wrong

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,47 @@
 <<<<<<< refs/remotes/origin/main
 @extends('layout')
 @section('content')
+<<<<<<< refs/remotes/origin/main
     <p>Hello Worlds</p>
+=======
+        <h2 class="title">Overview</h2>
+
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Location</th>
+                <th>food</th>
+                <th>image</th>
+                <th>price</th>
+                <th>start-time</th>
+                <th>end-time</th>
+            </tr>
+            <tr>
+                <td>Keagan Mulder</td>
+                <td>Amsterdam</td>
+                <td>Pizza Pollo</td>
+                <td><img id="pp" src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"></td>
+            </tr>
+            <tr>
+                <td>Gijs Pampiermole</td>
+                <td>Arnhem</td>
+                <td>Kaas</td>
+                <td><img id="pp" src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"></td>
+            </tr>
+            <tr>
+                <td>Tijme Wolters</td>
+                <td>Eindhoven</td>
+                <td>Maaltijdsalade Geitenkaas</td>
+                <td><img id="pp" src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"></td>
+            </tr>
+            <tr>
+                <td>Ben Berkenbosch</td>
+                <td>London</td>
+                <td>Lasagne Met Courgette</td>
+                <td><img id="pp" src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"></td>
+            </tr>
+        </table>
+>>>>>>> Backup Commit, as contingency when things go wrong
 @endsection
 =======
 <!DOCTYPE html>
