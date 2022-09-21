4e17c4801f6eb73a9a73efc46343936c35186271
Ban <ben.berkenbosch@student.graafschapcollege.nl>
2022-09-21 12:27:09 +0200

    Tables with info, can be connected to the database.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,5 @@
-<<<<<<< refs/remotes/origin/main
 @extends('layout')
 @section('content')
-<<<<<<< refs/remotes/origin/main
-    <p>Hello Worlds</p>
-=======
         <h2 class="title">Overview</h2>
 
         <table>
@@ -20,46 +16,40 @@
                 <td>Keagan Mulder</td>
                 <td>Amsterdam</td>
                 <td>Pizza Pollo</td>
-                <td><img id="pp" src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"></td>
+                <td><img class="pp" src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"></td>
+                <td>€22,50</td>
+                <td>2:45pm</td>
+                <td>3:15pm</td>
             </tr>
             <tr>
                 <td>Gijs Pampiermole</td>
                 <td>Arnhem</td>
                 <td>Kaas</td>
-                <td><img id="pp" src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"></td>
+                <td><img class="pp" src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"></td>
+                <td>€29,99</td>
+                <td>6:00am</td>
+                <td>7:00am</td>
             </tr>
             <tr>
                 <td>Tijme Wolters</td>
                 <td>Eindhoven</td>
                 <td>Maaltijdsalade Geitenkaas</td>
-                <td><img id="pp" src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"></td>
+                <td><img class="pp" src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"></td>
+                <td>€8,14</td>
+                <td>2:30pm</td>
+                <td>9:00pm</td>
             </tr>
             <tr>
                 <td>Ben Berkenbosch</td>
                 <td>London</td>
                 <td>Lasagne Met Courgette</td>
-                <td><img id="pp" src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"></td>
+                <td><img class="pp" src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"></td>
+                <td>€340,00</td>
+                <td>1:00pm</td>
+                <td>11:00am</td>
             </tr>
         </table>
->>>>>>> Backup Commit, as contingency when things go wrong
+
 @endsection
-=======
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    </head>
-    <body class="antialiased">
-        <div id="container">
-         <h2 class="title">Overview</h2>
 
-            <p>test</p>
-        </div>
-    </body>
-</html>
 @vite(['resources/css/app.scss', 'resources/js/app.js'])
-
->>>>>>> Added Flexbox
