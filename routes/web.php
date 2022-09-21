6c659f010083dd04d04e3750f329d99f66c106db
Keagan <keygun2k1@gmail.com>
2022-09-21 13:22:29 +0200

    Added missing controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Controller;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\LoggedIn;
