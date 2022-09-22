d62d32509e3d705da7e9025712906729e28e8e99
TijmeW <tijme.wolters@student.graafschapcollege.nl>
2022-09-22 11:35:28 +0200

    activities page

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,11 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 class PageController extends BaseController
 {
     public function loginPage(): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
     {
         return view('login');
+    }
+
+    public function activities(Request $request) {
+        $activities = DB::table('activities')->get();
+        return view('activities', ['activities' => $activities]);
+    }
+
+    public function addActivities(){
+        return view('addActivities');
     }
 }
