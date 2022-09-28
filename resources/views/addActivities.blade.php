f463c2e48a1d95fdcbbf8bb9b653c2de224d88a6
TijmeW <tijme.wolters@student.graafschapcollege.nl>
2022-09-28 13:34:39 +0200

    requirments + styling add form

--- resources/views/addActivities.blade.php
+++ resources/views/addActivities.blade.php
@@ -1,53 +1,66 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add activity</title>
-</head>
-<body>
-    <h1>Add</h1>
+@extends('layout')
+@section('content')
+    <h3 style="margin-top: 50px;">Add Activity</h3>
     <form action="/addActivity" method="POST">
         @csrf
-        <label for="name">name *</label>
-        <input type="text" name="name" id="name" maxlength="20" required>
+        <div class="col-lg-4">
+        <label for="name">Name <span style="color: red;">*</span></label><br>
+        <input type="text" name="name" id="name" maxlength="20" class="form-control" required>
         <br>
-        <label for="location">location *</label>
-        <input type="text" name="location" id="location" maxlength="20" required>
+        </div>
+        <div class="col-lg-4">
+        <label for="description">Description <span style="color: red;">*</span></label><br>
+        <textarea name="description" id="description" rows="6" maxlength="500" class="form-control" required></textarea>
         <br>
-        <label for="food">food *</label>
-        <select name="food" id="food" required>
+        </div>
+        <div class="col-lg-4">
+        <label for="image">Image <span style="color: red;">*</span></label><br>
+        <input type="text" name="image" id="image" maxlength="50" class="form-control" required>
+        <br>
+        </div>
+        <div class="col-lg-2">
+        <label for="price">Price <span style="color: red;">*</span></label><br>
+        <input type="number" name="price" id="price" min="1" max="10000" class="form-control" required>
+        <br>
+        </div>
+        <div class="col-lg-4">
+        <label for="needs">Needs <span style="color: red;">*</span></label><br>
+        <input type="text" name="needs" id="needs" maxlength="200" class="form-control" required>
+        <br>
+        </div>
+        <div class="col-lg-4">
+        <label for="location">Location <span style="color: red;">*</span></label><br>
+        <input type="text" name="location" id="location" maxlength="20" class="form-control" required>
+        <br>
+        </div>
+        <div class="col-lg-2">
+        <label for="food">Food <span style="color: red;">*</span></label><br>
+        <select name="food" id="food" class="form-select" required>
             <option value="yes">Yes</option>
             <option value="no">No</option>
         </select>
         <br>
-        <label for="image">image *</label>
-        <input type="text" name="image" id="image" maxlength="50" required>
+        </div>
+        <div class="col-lg-4">
+        <label for="startTime">Start time <span style="color: red;">*</span></label><br>
+        <input type="datetime-local" name="startTime" id="startTime" class="form-control" required>
         <br>
-        <label for="price">price *</label>
-        <input type="number" name="price" id="price" min="1" max="10000" required>
+        </div>
+        <div class="col-lg-4">
+        <label for="endTime">End time <span style="color: red;">*</span></label><br>
+        <input type="datetime-local" name="endTime" id="endTime" class="form-control" required>
         <br>
-        <label for="startTime">Start time *</label>
-        <input type="datetime-local" name="startTime" id="startTime" required>
+        </div>
+        <div class="col-lg-4">
+        <label for="minParticipants">Minimum participants <span style="color: red;">*</span></label><br>
+        <input type="number" name="minParticipants" id="minParticipants" min="1" max="1000" class="form-control" required> 
         <br>
-        <label for="endTime">End time *</label>
-        <input type="datetime-local" name="endTime" id="endTime" required>
+        </div>
+        <div class="col-lg-4">
+        <label for="maxParticipants">Maximum participants <span style="color: red;">*</span></label><br>
+        <input type="number" name="maxParticipants" id="maxParticipants" min="1" max="1000" class="form-control" required>
         <br>
-        <label for="description">description *</label>
-        <input type="text" name="description" id="description" maxlength="500" required>
-        <br>
-        <label for="needs">needs *</label>
-        <input type="text" name="needs" id="needs" maxlength="200" required>
-        <br>
-        <label for="maxParticipants">max participants *</label>
-        <input type="number" name="maxParticipants" id="maxParticipants" min="1" max="1000" required>
-        <br>
-        <label for="minParticipants">min participants *</label>
-        <input type="number" name="minParticipants" id="minParticipants" min="1" max="1000" required> 
-        <br>
-        <button type="submit">Add</button>
+        </div>
+        <button type="submit" class="btn btn-primary" style="margin-bottom: 50px;">Add</button>
     </form>
-
-</body>
-</html>
+@endsection
