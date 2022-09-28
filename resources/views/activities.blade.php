5ccd23886ad92e4ba1c0ab223d659cbb27021ac9
GijsPampiermole <gpampiermole@gmail.com>
2022-09-28 15:10:03 +0200

    Changes to be committed: 	modified:   resources/css/app.scss 	modified:   resources/views/activities.blade.php 	modified:   resources/views/welcome.blade.php  Changes not staged for commit: 	modified:   resources/css/app.scss

--- resources/views/activities.blade.php
+++ resources/views/activities.blade.php
@@ -1,20 +1,31 @@
 @extends('layout')
 @section('content')
     <p>Activities</p>
-    @foreach($activities as $activity)
-        <div class="activity">
-            <h2>{{ $activity->name }}</h2>
-            <p>{{ $activity->description }}</p>
-            <p>{{ $activity->location }}</p>
-            <p>{{ $activity->startTime }}</p>
-            <p>{{ $activity->endTime }}</p>
-            <p>{{ $activity->price }}</p>
-            <p>{{ $activity->food }}</p>
-            <p>{{ $activity->needs }}</p>
-            <p>{{ $activity->maxParticipants }}</p>
-            <p>{{ $activity->minParticipants }}</p>
+
+    <div class="accordion">
+        @foreach($activities as $activity)
+        <div class="header" id="accordion-header">
+            <div class="left">27/01</div>
+            <div class="middle">{{ $activity->name }}</div>
+            <div class="end">
+                <img src="down-arrow-5.svg" alt="" class="svg-dropdown">
+            </div>
         </div>
-    @endforeach
+        <div class="content" id="accordion-content" style="max-height: 0px;">
+            <div class="image"><img src="https://via.placeholder.com/150" alt=""></div>
+            <div class="location">{{ $activity->location }}</div>
+            <div class="food">{{ $activity->food }}</div>
+            <div class="price">{{ $activity->price }}</div>
+            <div class="start-time">{{ $activity->startTime }}</div>
+            <div class="end-time">{{ $activity->endTime }}</div>
+            <div class="description">{{ $activity->description }}</div>
+            <div class="needs">{{ $activity->needs }}</div>
+            <div class="participants">{{ $activity->minParticipants }} - {{ $activity->maxParticipants }} participants</div>
+            <div class="current-participants">6 participants</div>
+        </div>
+        @endforeach
+    </div>
+    
     <button id="btn">
         <a href="/addActivities">Add activity</a>
     </button>
